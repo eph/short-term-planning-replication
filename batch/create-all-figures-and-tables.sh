159829c075ab9b014cbb08e42e796669c9bf88c7
Ed Herbst <ed.herbst@gmail.com>
2021-01-05 13:24:59 -0500

    add batch scripts

---
+++ batch/create-all-figures-and-tables.sh
@@ -0,0 +1,10 @@
+python scripts/04-fig-impulse-response-disinflation-shock.py             
+python scripts/05-tab-posterior.py                                       
+python scripts/06-fig-joint-posterior.py                                 
+python scripts/07-fig-difference-log-mdd-time.py                         
+python scripts/08-fig-impulse-response-monetary-shock.py                 
+python scripts/09-fig-trend-cycle-decomposition.py                       
+python scripts/10-fig-estimated-shocks.py                                
+python scripts/11-fig-trend-cycle-counterfactual.py                      
+python scripts/12-tab-log-mdd-single-agents.py                           
+python scripts/13-tab-log-mdd-alternative-models.py                      
