9a2559b7298a3466064c28c7d714ff6c99c006f9
Ed Herbst <ed.herbst@gmail.com>
2021-03-12 08:19:02 -0500

    small changes

--- prepare-for-aej-submission.sh
+++ prepare-for-aej-submission.sh
@@ -5,9 +5,10 @@ pandoc --to=markdown README.org >> README.md
 zip -r short-term-planning-replication.zip README.md env.yaml single_agent_template.f90
 zip short-term-planning-replication.zip scripts/* -x scripts/__pycache__/
 zip short-term-planning-replication.zip scripts/* -x scripts/__pycache__/
+zip short-term-planning-replication.zip figures-tables/
 zip short-term-planning-replication.zip batch/*
 zip short-term-planning-replication.zip data/*
 zip -r short-term-planning-replication.zip models/*
 zip -r short-term-planning-replication.zip fortran/*/output*
-zip  short-term-planning-replication.zip fortran/canonical_NK/time-posteriors/*
-zip  short-term-planning-replication.zip fortran/finite_horizon_phibar/time-posteriors/*
+zip short-term-planning-replication.zip fortran/canonical_NK/time-posteriors/*
+zip short-term-planning-replication.zip fortran/finite_horizon_phibar/time-posteriors/*
