84150765d260aa55f1fc8dddd757578f4a503d64
Ed Herbst <ed.herbst@gmail.com>
2021-03-11 19:07:22 -0500

    add batch and data to aej-export

--- prepare-for-aej-submission.sh
+++ prepare-for-aej-submission.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 rm -f short-term-planning-replication.zip
+rm -f README.md
 pandoc --to=markdown README.org >> README.md
 zip -r short-term-planning-replication.zip README.md env.yaml single_agent_template.f90
 zip short-term-planning-replication.zip scripts/* -x scripts/__pycache__/
-zip short-term-planning-replication.zip figures-tables
+zip short-term-planning-replication.zip scripts/* -x scripts/__pycache__/
+zip short-term-planning-replication.zip batch/*
+zip short-term-planning-replication.zip data/*
 zip -r short-term-planning-replication.zip models/*
 zip -r short-term-planning-replication.zip fortran/*/output*
 zip  short-term-planning-replication.zip fortran/canonical_NK/time-posteriors/*
